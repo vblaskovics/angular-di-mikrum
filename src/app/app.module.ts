95cb56fd20f6693feb2c932b2c8857fa0224f8d4
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-08 15:44:48 +0200

    Add deps to provider factory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AnalyticsDemoComponent } from './analytics-demo/analytics-demo.component';
 import { AnalyticsImplementation, Metric } from './analytics-demo/analytics-demo.interface';
@@ -15,16 +16,22 @@ import { UserComponent } from './user/user.component';
     AnalyticsDemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [
     {provide: UserService, useClass: UserService},
     {provide: 'API_URL', useValue: 'http://test.api.com/v1'},
-    
-    {provide: AnalyticsService, useFactory() {
+
+    {provide: AnalyticsService,
+      deps: [HttpClient, 'API_URL'],
+      useFactory(httpClient: HttpClient, apiUrl: string) {
       const loggingImplementation: AnalyticsImplementation = {
         recordEvent: (metric: Metric):void => {
-          console.log('The metric is:', metric)
+          console.log('The metric is:', metric);
+          console.log('Sending metric to:', apiUrl);
+          // ... send metric to the given API
+          // httpClient.post ...
         }
       }
 
